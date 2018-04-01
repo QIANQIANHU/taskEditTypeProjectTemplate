2364d9061f3189211f53306df420d0b5529de70e
Qianqian Hu <qianhu@pindous.com>
2018-04-01 11:09:00 -0700

    create template follow the course

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { FormsModule }  from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
